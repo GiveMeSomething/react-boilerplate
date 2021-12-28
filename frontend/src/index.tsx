e5d0d7d15a3ca06f763c34570fd57b699103a19c
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-12-28 09:31:32 +0700

    chore: remove unecessary meta tag

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'semantic-ui-css/semantic.min.css'
 import './styles/global.scss'
-
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-
 import Routes from 'routes'
 
-ReactDOM.render(
-    <Provider store={store}>
-        <Routes />
-    </Provider>,
-    document.getElementById('root')
-)
+ReactDOM.render(<Routes />, document.getElementById('root'))
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import Signin from 'pages/auth/Signin'
-import Signup from 'pages/auth/Signup'
-import ServersPage from 'pages/server/Server'
-
 const Routes = () => {
     return (
         <Router>
