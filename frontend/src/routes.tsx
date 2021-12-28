444660358ed7f1abb173f80de78c03acfa5d793b
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-12-28 11:04:51 +0700

    feat: create a landing page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react'
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
@@ -6,8 +7,122 @@ const Routes = () => {
         <Router>
             <Switch>
                 <Route path="/">
-                    <div className="w-screen h-screen flex items-center justify-center">
-                        <h1>Hello World</h1>
+                    <div className="w-screen h-screen container flex flex-col items-center justify-center">
+                        <p className="text-6xl text-blue-500 p-6">
+                            My React Boilerplate
+                        </p>
+                        <div className="grid grid-cols-3 gap-4">
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="logos:react"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            React
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        A JavaScript library for building user
+                                        interfaces
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="logos:redux"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            Redux
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        A Predictable State Container for JS
+                                        Apps
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="logos:eslint"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            ESLint
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        Find and fix problems in your JavaScript
+                                        code
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="logos:prettier"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            Prettier
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        An opinionated code formatter
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="simple-icons:tailwindcss"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            TailwindCSS
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        A cool utility-first CSS framework
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="border-2 border-blue-300 rounded-2xl">
+                                <div className="flex flex-col items-center justify-center px-8 py-4">
+                                    <div className="flex items-center">
+                                        <Icon
+                                            icon="simple-icons:iconify"
+                                            className="text-4xl m-2"
+                                        />
+                                        <p className="text-4xl text-blue-500">
+                                            Iconify
+                                        </p>
+                                    </div>
+
+                                    <p className="text-center">
+                                        Unified icons framework
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Route>
             </Switch>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,91 +1,14 @@
-const colors = require('tailwindcss/colors')
 const aspectRatio = require('@tailwindcss/aspect-ratio')
 
 module.exports = {
-    important: true,
     mode: 'jit',
-    purge: ['./src/**/*.tsx'],
-    darkMode: false, // or 'media' or 'class'
+    purge: ['./public/**/*.html', './src/**/*.tsx'],
     theme: {
-        colors: {
-            transparent: 'transparent',
-            current: 'currentColor',
-            black: colors.black,
-            white: colors.white,
-            gray: colors.trueGray,
-            indigo: colors.indigo,
-            red: colors.rose,
-            yellow: colors.amber,
-            fresh: {
-                1: {
-                    100: '#030078',
-                    200: '#001F82',
-                    300: '#05386B',
-                    400: '#006382',
-                    500: '#007874'
-                },
-                2: {
-                    100: '#347DA3',
-                    200: '#37A4AD',
-                    300: '#379683',
-                    400: '#37AD74',
-                    500: '#34A34E'
-                },
-                3: {
-                    100: '#56E6E8',
-                    200: '#5AF2CA',
-                    300: '#5CDB95',
-                    400: '#5AF273',
-                    500: '#6BE856'
-                },
-                4: {
-                    100: '#89F0EA',
-                    200: '#8EFAD6',
-                    300: '#8EE4AF',
-                    400: '#8EFA99',
-                    500: '#A0F089'
-                },
-                5: {
-                    100: '#DAFADC',
-                    200: '#C8DEC1',
-                    300: '#EDF5E1',
-                    400: '#DCDEC1',
-                    500: '#FAF8DA'
-                }
-            },
-            slack: {
-                navbar: '#350D36',
-                sidebar: {
-                    normal: '#3F0E40',
-                    hover: '#350D36',
-                    focus: '#1164A3',
-                    blur: '#BCABBC'
-                },
-                text: {
-                    focus: '#FFFFFF',
-                    blur: '#755775',
-                    dark: '#1D1C1D'
-                },
-                searchbar: '#644565'
-            }
-        },
-        extend: {
-            colors: {
-                green: {
-                    compound: {
-                        1: '#2DC4ED',
-                        2: '#294B54',
-                        3: '#C34942',
-                        4: '#ED2D6B'
-                    }
-                }
-            }
-        }
+        extend: {}
     },
     variants: {
-        extend: {
-            opacity: ['disabled']
-        }
+        extend: {}
     },
+    darkMode: false, // or 'media' or 'class'
     plugins: [aspectRatio]
 }
