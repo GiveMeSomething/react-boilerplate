0c38cd0cde42bfcc6097b0f5be12dcaa4738a9a8
GiveMeSomething <minhhthe150277@fpt.edu.vn>
2021-12-28 11:45:34 +0700

    feat: update React to v5.0.0, TailwindCSS to v3.0.5

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,7 @@
-const aspectRatio = require('@tailwindcss/aspect-ratio')
-
 module.exports = {
-    purge: ['./public/**/*.html', './src/**/*.tsx'],
+    content: ['./public/**/*.html', './src/**/*.tsx'],
     theme: {
         extend: {}
     },
-    variants: {
-        extend: {}
-    },
-    darkMode: false, // or 'media' or 'class'
-    plugins: [aspectRatio]
+    plugins: []
 }
